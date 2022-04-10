5d8f2a132ff4a8cb6fa7ac58e00b65c4474e3e5a
tomcatoo <ahmad.msadae@gmail.com>
2022-04-10 04:12:56 +0300

    IProperties

--- Zoo/Zoo/Classes/AnimalProperties.cs
+++ Zoo/Zoo/Classes/AnimalProperties.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Zoo.Classes
 {
-    public abstract class AnimalProperties : AnimalCage,IAnimal
+    public abstract class AnimalProperties : AnimalCage,IAnimal, IProperties
     {
        
         public virtual string animalName { get; set; }
---
+++ Zoo/Zoo/Classes/IProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Classes
+{
+    interface IProperties
+    {
+        string animalName { get; set; }
+        string eats { get; set; }
+        bool isPredatory { get; set; }
+        bool layEggs { get; set; }
+    }
+}
